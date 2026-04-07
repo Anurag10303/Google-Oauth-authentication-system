af8681d2205220121d676ca6bb4f6024df98b7e8
Anurag10303 <anuragsingh22230@gmail.com>
2026-04-07 21:45:37 +0530

    changes in Css

--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  darkMode: "class",
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // ✅ ADD THIS
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}", // optional
-  ],
-};
